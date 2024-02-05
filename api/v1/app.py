a312e59ad03aa2413aa48c77d632c89eb5d99d58
simoIdbrahim <mohamedidbrahim16@gmail.com>
2024-02-05 15:19:27 -0800

    add index page and run

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""
-This file is the main entry point
-for the AirBnB clone version 3 API.
-"""
+""" flask app """
 
 
 from api.v1.views import app_views
@@ -19,17 +16,17 @@ CORS(app, resources={r"/*": {"origins": "0.0.0.0"}})
 
 @app.errorhandler(404)
 def notFound(err):
-    """ handler error 404 """
+    """ 404 err code status"""
     return jsonify({"error": "Not found"}), 404
 
 
 @app.teardown_appcontext
 def teardown_db(exception=None):
-    """ Close the database """
+    """ end database"""
     storage.close()
 
 
 if __name__ == "__main__":
-    HBNB_HOST = getenv("HBNB_API_HOST", "0.0.0.0")
-    HBNB_PORT = getenv("HBNB_API_PORT", 5000)
-    app.run(host=HBNB_HOST, port=HBNB_PORT, debug=True, threaded=True)
+    host = getenv("HBNB_API_HOST", "0.0.0.0")
+    port = getenv("HBNB_API_PORT", 5000)
+    app.run(host=host, port=host, debug=True, threaded=True)
--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-"""App views for AirBnB_clone_v3
-
-Indexing app views
-"""
+""" flask index page """
 
 from flask import jsonify
 from models import storage
@@ -11,24 +8,14 @@ from api.v1.views import app_views
 
 @app_views.route('/status')
 def status():
-    """
-    Returns the status of the API.
-
-    :return: A JSON object containing the status.
-    """
+    """ check app status its ok """
     status = {"status": "OK"}
     return jsonify(status)
 
 
 @app_views.route('/stats')
 def count():
-    """
-    Returns the number of each object by type.
-
-    Returns:
-        A JSON object containing the count of each object type.
-
-    """
+    """ added count() method from storage """
     alls = {}
     classes = {"Amenity": "amenities",
                "City": "cities",
@@ -36,7 +23,7 @@ def count():
                "Review": "reviews",
                "State": "states",
                "User": "users"}
-    for classx in classes:
-        count = storage.count(classx)
-        alls[classes.get(classx)] = count
+    for cls in classes:
+        count = storage.count(cls)
+        alls[classes.get(cls)] = count
     return jsonify(alls)
